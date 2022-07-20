43b7fe102eb1b5529dadc563a81b6bf823554a3c
Neno <eugenio.bufalorodrigues@gmail.com>
2022-07-19 22:09:00 -0300

    CORS Habilitado

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { env } from 'process';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
 
